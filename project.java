686292eee95783a7910b2f6ffa36d47c9584e471
Daut Baterbiev <baterbievd@gmail.com>
2022-08-18 10:16:09 -0400

    Java basics

--- project.java
+++ project.java
@@ -1,5 +1,20 @@
-public class project {
+// class name Project 
+public class Project {
+   // inside this man method every code will be exicuted
  public static void main(String[] args) {
+   // println method to output values or text in Java
     System.out.println("Hello World");
+    //creating a variable of type String must be surronded by double quotes " "
+    String name = "Meredith";
+    System.out.println("Hello, " + name);
+    // creating a variable of type integer, must be a whole number
+
+    int x = 10;
+    System.out.println("Hello my name is " + name + " and i'm " + x + " years old!");
+    int y = 3;
+    System.out.println(name + " has a dog, and he's " + y + " years old!");
+    // summing Meredith's and Dog's ages
+    System.out.println(x + y);
+
  }
 }
