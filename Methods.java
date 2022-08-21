de938b1fc229809128612155ed01917a965ddd38
Daut baterbiev <baterbievd@gmail.com>
2022-08-21 13:07:20 -0400

    New file

---
+++ Methods.java
@@ -0,0 +1,15 @@
+public class Methods {
+    static void myMethode(){ // static means that the method belongs to the main class, void means that method doesn't have a return value.
+    System.out.println("learning some methods");
+
+    public static void main(String[] args){
+        //Jsvs Methods
+        // A method is block of code which only runs when it is called.
+        // We can pass data, known as parameters, into a method.
+        // We use methods to perform certain actions, they are also known as functions.
+        // With methods we define a code once and may reuse many times.
+       myMethod();
+
+        }
+    }
+}
